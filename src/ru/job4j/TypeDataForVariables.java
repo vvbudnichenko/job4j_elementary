5f29a230a97a5eb1cb5fa41f55530e91d9ce5713
Vladimir <budnichik@gmail.com>
2020-04-10 13:44:17 +0300

    Что такое переменная.[#270520]

---
+++ src/ru/job4j/TypeDataForVariables.java
@@ -0,0 +1,28 @@
+package ru.job4j;
+
+public class TypeDataForVariables {
+    public static void main(String[]args){
+        int age;
+        char man, woman;
+        short height;
+        long masonry, virus;
+        float space_length, file_size;
+        boolean work_visa;
+        age = 121;
+        man = 'm';
+        woman = 'w';
+        height = 195;
+        masonry = 123579545L;
+        virus = 2147483648L;
+        space_length = 22156595411452452f;
+        file_size = 3.2f;
+        System.out.println("Your age is " + age);
+        System.out.println("You are " + man);
+        System.out.println("You are " + woman);
+        System.out.println("Your heigth is " + height);
+        System.out.println("Quantity of masonry is " + masonry);
+        System.out.println("Quantity of virus is " + virus);
+        System.out.println("Length to the object is about " + space_length);
+        System.out.println("Size of file is " + file_size);
+    }
+}

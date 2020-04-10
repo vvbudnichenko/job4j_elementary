b3ee07b289d0d3e04d9540f1f1f565bca5e2ce0a
Vladimir <budnichik@gmail.com>
2020-04-10 11:33:27 +0300

    Что такое переменная.[#270520]

---
+++ src/ru/job4j/WhaIsVariable.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class WhaIsVariable {
+  public static void main(String[]args){
+      int age = 18;
+      float length = 78.0332f;
+      boolean baby = true;
+      System.out.println(age);
+      System.out.println(length);
+      System.out.println(baby);
+  }
+}

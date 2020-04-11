098b395e2bd03484fa4e8e8c90bc7966dfa91e95
Vladimir <budnichik@gmail.com>
2020-04-11 22:02:02 +0300

    String[#270517]

---
+++ src/ru/job4j/condition/Greating.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Greating {
+    public static void main(String[]args){
+        String idea = "I like Java! ";
+        System.out.println(idea);
+        System.out.println(idea + "But I am a newbie.");
+        int year = 2020;
+        idea = idea + year;
+        System.out.println(idea);
+    }
+}

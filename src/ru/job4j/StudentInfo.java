406cd9ca6957f9973ed38956d74022c61a7e7b51
Vladimir <budnichik@gmail.com>
2020-04-12 11:53:48 +0300

    StudentInfo

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+import javax.swing.*;
+
+public class StudentInfo {
+    public static void main(String[]args) {
+        System.out.println("Vladimir Budnichenko");
+        System.out.println("31.03.1988");
+        JOptionPane.showMessageDialog(null, "Minä olen Vladimir. Olen 32 vuotta votias");
+    }
+}

37badc092487309dda543f719ef04ce7d271f7ae
Vladimir <budnichik@gmail.com>
2020-04-12 11:04:56 +0300

    Конвертер валюты [#41583]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,15 +1,15 @@
 package ru.job4j.converter;
 
 public class Converter {
-    public static int rubleToEuro(int value1){
+    public static int rubleToEuro(int value1) {
         int rsl = value1 / 70;
         return rsl;
     }
-    public static int rubleToDollar(int value2){
+    public static int rubleToDollar(int value2) {
         int rsl2 = value2 / 60;
         return rsl2;
     }
-    public static void main(String[]args){
+    public static void main(String[]args) {
         int rsl = Converter.rubleToEuro(7000);
         System.out.println("При данном курсе валют сумма размерна на евро равна " + rsl);
         int rs2 = Converter.rubleToDollar(7000);

5996d83a1c1a9e5c6ca7cadb61c2317f00301335
Vladimir <budnichik@gmail.com>
2020-04-12 11:09:47 +0300

    String [#237407]

--- src/ru/job4j/condition/Greating.java
+++ src/ru/job4j/condition/Greating.java
@@ -2,11 +2,11 @@ package ru.job4j.condition;
 
 public class Greating {
     public static void main(String[]args) {
-        String idea = "I like Java! ";
-        System.out.println(idea);
-        System.out.println(idea + "But I am a newbie.");
+        String idea1 = "I like Java! ";
+        System.out.println(idea1);
+        System.out.println(idea1 + "But I am a newbie.");
         int year = 2020;
-        idea = idea + year;
-        System.out.println(idea);
+        idea1 = idea1 + year;
+        System.out.println(idea1);
     }
 }

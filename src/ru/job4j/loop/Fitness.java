61fa184f836335598bdf2005c3e526304ec06c16
Vladimir <budnichik@gmail.com>
2020-04-23 21:02:07 +0300

    5.4. Протеиновая диета.[#270556]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -2,24 +2,20 @@ package ru.job4j.loop;
 
 public class Fitness {
     public static int calc(int ivan, int nik) {
-        boolean th = true;
         int month = 0;
         if (ivan > nik) {
             return month;
         }
         ivan *= 3;
         nik *= 2;
-        while (th) {
+        for (month = 0; ;) {
             month++;
             ivan *= month;
             nik *= month;
-            if (ivan > nik) {
+            while (ivan > nik) {
                 return month;
             }
         }
-        return month;
     }
 }
-
-
 

dfac52a2ccfcba0ab948fa71dc507a359df85c53
Vladimir <budnichik@gmail.com>
2020-05-31 18:15:15 +0300

    2. Перегрузка конструктора. [#270655]

--- src/ru/job4j/tracker/Animal.java
+++ src/ru/job4j/tracker/Animal.java
@@ -3,22 +3,18 @@ package ru.job4j.tracker;
 public class Animal {
     public Animal() {
     }
-
+    public Animal(String name) {
+    }
     public static class Predator extends Animal {
-        public Predator() {
+        public Predator(String name) {
+            super(name);
         }
     }
-
     public static class Tiger extends Animal {
-        public Tiger() {
+        public Tiger(String name) {
+            super(name);
         }
     }
     public static void main(String[] args) {
-        Animal animal = new Animal();
-        System.out.println(animal);
-        Predator predy = new Predator();
-        System.out.println(predy);
-        Tiger tigy = new Tiger();
-        System.out.println(tigy);
     }
 }

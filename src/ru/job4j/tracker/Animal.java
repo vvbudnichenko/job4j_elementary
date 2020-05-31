fe4bf5a087d540fe0b8fe271d3428692d8235676
Vladimir <budnichik@gmail.com>
2020-05-31 17:36:01 +0300

    1. Конструктор по умолчанию. [#270654]

--- src/ru/job4j/tracker/Animal.java
+++ src/ru/job4j/tracker/Animal.java
@@ -2,18 +2,15 @@ package ru.job4j.tracker;
 
 public class Animal {
     public Animal() {
-        System.out.println();
     }
 
     public static class Predator extends Animal {
         public Predator() {
-            System.out.println();
         }
     }
 
     public static class Tiger extends Animal {
         public Tiger() {
-            System.out.println();
         }
     }
     public static void main(String[] args) {

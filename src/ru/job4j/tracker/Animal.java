e867a96576399b06287470a80a327dac8c2bc690
Vladimir <budnichik@gmail.com>
2020-06-01 12:30:25 +0300

    2. Перегрузка конструктора. [#270655]

--- src/ru/job4j/tracker/Animal.java
+++ src/ru/job4j/tracker/Animal.java
@@ -2,6 +2,8 @@ package ru.job4j.tracker;
 
 public class Animal {
     public Animal() {
+    }
+    public Animal(String name) {
         System.out.println("Animal");
     }
 }
--- src/ru/job4j/tracker/Predator.java
+++ src/ru/job4j/tracker/Predator.java
@@ -1,7 +1,7 @@
 package ru.job4j.tracker;
 
 public class Predator extends Animal {
-    public Predator() {
+    public Predator(String name) {
         super();
         System.out.println("Predator");
     }

3ed34d97d241517f2ec30b09180c7db2cad8465b
Vladimir <budnichik@gmail.com>
2020-06-06 18:54:42 +0300

    4. Переопределение [#270656]

--- src/ru/job4j/inheritance/JSONReport.java
+++ src/ru/job4j/inheritance/JSONReport.java
@@ -1,8 +1,9 @@
 package ru.job4j.inheritance;
 
+
 public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
-        return "If you sum \"" + name + "\"" + " and \"body\", " + "You will have " + "\"" + body + "\"";
+        return "{" + System.lineSeparator() + "\"name\" : " + "\"" + name + "\"," + System.lineSeparator() + "\"body\" : " + "\"" + body + "\"" + System.lineSeparator() + "}";
     }
 }

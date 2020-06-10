676aa43adaf4c4f9a49719f1058ebc7e854f2a53
Vladimir <budnichik@gmail.com>
2020-06-10 14:27:47 +0300

    3. Массивы и модели. [#270662]

--- src/ru/job4j/pojo/Shop.java
+++ src/ru/job4j/pojo/Shop.java
@@ -1,11 +1,11 @@
 package ru.job4j.pojo;
 
+import junior.Products;
 import org.junit.Assert;
 
-
 public class Shop {
-    public static void main(String[] args) {
-       /* Product milk = new Product("Milk", 10);
+    /*public static void main(String[] args) {
+        Product milk = new Product("Milk", 10);
         Product bread = new Product("Bread", 4);
         Product egg = new Product("Egg", 19);
 
@@ -44,39 +44,39 @@ public class Shop {
                 System.out.println("null");
             }
         }
-        System.out.println();*/
+        System.out.println();
         Product cola = new Product("Cola", 10);
         Product vesi = new Product("Vesi", 4);
         Product kvas = new Product("Kvas", 19);
 
         int entery = 5;
         Product[] prody = new Product[entery];
-        prody[2] = cola;
+        prody[0] = cola;
+        prody[2] = kvas;
         prody[3] = vesi;
-        prody[4] = kvas;
         Product[] expceted = new Product[entery];
         expceted[0] = cola;
-        expceted[1] = vesi;
-        expceted[2] = kvas;
-        expceted[3] = null;
-        expceted[4] = null;
+        expceted[1] = kvas;
+        expceted[2] = vesi;
         Product[] pr = delete(prody, entery);
         Assert.assertArrayEquals(pr, expceted);
-    }
-
+    }*/
     public static Product[] delete(Product[] products, int index) {
         for (index = 0; index < products.length - 1; index++) {
-            Product pr = products[index];
-            if (pr == null) {
-                products[index] = products[index + 1];
-                products[index + 1] = null;
-                if (pr != null) {
-                    System.out.println(pr.getName() + " - " + pr.getCount());
-                } else {
-                    System.out.println("null");
+            for (int i = index; i < products.length - 1; i++) {
+                if (products[i] == null) {
+                    Product temp = products[i + 1];
+                    products[i + 1] = products[i];
+                    products[i] = temp;
                 }
+            } products[products.length - 1] = null;
+            Product pr = products[index];
+            if (pr != null) {
+                System.out.println(pr.getName() + " " + pr.getCount());
+            } else {
+                    System.out.println("null");
             }
-        }
-            return products;
-        }
+        } System.out.println();
+        return products;
     }
+}
---
+++ test/ru/job4j/pojo/ShopTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.pojo;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class ShopTest {
+
+    @Test
+    public void whenItThenONe() {
+        Product cola = new Product("Cola", 10);
+        Product vesi = new Product("Vesi", 4);
+        Product kvas = new Product("Kvas", 19);
+        int entery = 5;
+        Product[] prody = new Product[entery];
+        prody[0] = cola;
+        prody[2] = kvas;
+        prody[3] = vesi;
+        Product[] expceted = new Product[entery];
+        expceted[0] = cola;
+        expceted[1] = kvas;
+        expceted[2] = vesi;
+        Product[] pr = Shop.delete(prody, entery);
+        Assert.assertArrayEquals(pr, expceted);
+    }
+    @Test
+    public void whenItThenThree() {
+        Product cola = new Product("Cola", 10);
+        Product vesi = new Product("Vesi", 4);
+        Product kvas = new Product("Kvas", 19);
+        int entery = 5;
+        Product[] prody = new Product[entery];
+        prody[0] = cola;
+        prody[3] = kvas;
+        prody[4] = vesi;
+        Product[] expceted = new Product[entery];
+        expceted[0] = cola;
+        expceted[1] = kvas;
+        expceted[2] = vesi;
+        expceted[3] = null;
+        expceted[4] = null;
+        Product[] pr = Shop.delete(prody, entery);
+        Assert.assertArrayEquals(pr, expceted);
+    }
+}

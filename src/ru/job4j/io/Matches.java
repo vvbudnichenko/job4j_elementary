e5f83848eb572bdb51ca8b64b1f4e80388fbe3df
Vladimir <budnichik@gmail.com>
2020-06-26 18:50:17 +0300

    2. Scanner и чтение числа из консоли. [#270605]

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -3,32 +3,28 @@ package ru.job4j.io;
 import java.util.Scanner;
 
 public class Matches {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean player = true;
         int total = 11;
-        int step;
-        System.out.println("Игра началась тяни списку");
-        while (true) {
-            System.out.print("Ходит первый игрок: ");
-            do {
-                step = Integer.valueOf(input.nextLine());
-            } while (step > 3 || step < 1);
-            total -= step;
-            if (total <= 0) {
-                System.out.println("Победил первый игрок");
-                break;
+        System.out.println("Игра началась. Всего 11 спичек.");
+        while (total > 0) {
+            System.out.print("Тяни 1, 2 или 3 спички: ");
+            int step = Integer.valueOf(input.nextLine());
+            player = !player;
+            if (step > 3 || step < 1) {
+                continue;
+            } else if (total - step <= 0) {
+                if (player) {
+                    System.out.println("Первый игрок поведил!");
+                } else {
+                    System.out.println("Второй игрок победил!");
+                }
             }
-            System.out.println("Осталось " + total);
-            System.out.print("Ходит второй игрок: ");
-            do {
-                step = Integer.valueOf(input.nextLine());
-            } while (step > 3 || step < 1);
             total -= step;
-            if (total <= 0) {
-                System.out.println("Победил второй игрок");
-                break;
-            }
-            System.out.println("Осталось " + total);
+            System.out.println("Осталось " + total + " спичек");
+            System.out.println();
         }
     }
 }

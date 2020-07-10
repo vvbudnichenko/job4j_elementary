935106ae2fc876fcf526dbbc334bd51d6001dd96
Vladimir <budnichik@gmail.com>
2020-07-10 14:54:46 +0300

    4.2. Статические методы. [#270611]

--- src/ru/job4j/tracker/StartUI.java
+++ src/ru/job4j/tracker/StartUI.java
@@ -2,16 +2,39 @@ package ru.job4j.tracker;
 
 
 public class StartUI {
-    public void init(Input input, Tracker tracker) {
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("=== Create a new Item === ");
+        String name = input.askStr("Enter name: ");
+        ItemFive itemFive = new ItemFive(name);
+        tracker.add(itemFive);
+    }
+    public static void replaceItem(Input input, Tracker tracker) {
+        System.out.println("=== To edit an item do pushing === ");
+        String name = input.askStr("Enter name: ");
+        ItemFive newItem = new ItemFive(name);
+        int id = input.askInt("ID: ");
+        if (tracker.replace(id, newItem)) {
+            System.out.println("Item replaced");
+        } else {
+            System.out.println("Error");
+        }
+    }
+    public static void deleteItem(Input input, Tracker tracker) {
+        System.out.println("=== Delete items === ");
+        int id = input.askInt("Enter id: ");
+        if (tracker.delete(id)) {
+            System.out.println("Item deleted");
+        } else {
+            System.out.println("Error");
+        }
+    }
+        public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
             this.showMenu();
             int select = input.askInt("Select: ");
             if (select == 0) {
-                System.out.println("=== Create a new Item === ");
-                String name = input.askStr("Enter name: ");
-                ItemFive itemFive = new ItemFive(name);
-                tracker.add(itemFive);
+                StartUI.createItem(input, tracker);
             } else if (select == 1) {
                 System.out.println("=== Push to show all items === ");
                 ItemFive[] array = tracker.findAll();
@@ -20,23 +43,9 @@ public class StartUI {
                     System.out.println("Item: " + result.getName() + ". Id number : " + result.getId());
                 }
             } else if (select == 2) {
-                System.out.println("=== To edit an item do pushing === ");
-                String name = input.askStr("Enter name: ");
-                ItemFive newItem = new ItemFive(name);
-                int id = input.askInt("ID");
-                if (tracker.replace(id, newItem)) {
-                    System.out.println("Item replaced");
-                } else {
-                    System.out.println("Error");
-                }
+                StartUI.replaceItem(input, tracker);
             } else if (select == 3) {
-                System.out.println("=== Delete items === ");
-                int id = input.askInt("Enter id: ");
-                if (tracker.delete(id)) {
-                    System.out.println("Item deleted");
-                } else {
-                    System.out.println("Error");
-                }
+                StartUI.deleteItem(input, tracker);
             } else if (select == 4) {
                 System.out.println("=== Find ID === ");
                 int id = input.askInt("Enter id: ");

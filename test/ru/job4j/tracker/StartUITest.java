4b2dac12c222c35e4543d75cff573a0927d516eb
Vladimir <budnichik@gmail.com>
2020-07-18 16:45:23 +0300

    5.1. Тестирование. Подготовка данных. [#270613]

--- test/ru/job4j/tracker/StartUITest.java
+++ test/ru/job4j/tracker/StartUITest.java
@@ -15,15 +15,28 @@ public class StartUITest {
         ItemFive expected = new ItemFive("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
     }
+
     @Test
     public void whenReplaceItem() {
         Tracker tracker = new Tracker();
         ItemFive itemFive = new ItemFive("new item");
         tracker.add(itemFive);
-        tracker.findById(itemFive.getId());
-        String[] answers = {"replaced item"};
+        String[] answers = {"replaced item",
+                String.valueOf(itemFive.getId())
+        };
         StartUI.replaceItem(new StubInput(answers), tracker);
         ItemFive replaced = tracker.findById(itemFive.getId());
         assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        ItemFive itemFive = new ItemFive("new item");
+        tracker.add(itemFive);
+        String[] answers = {String.valueOf(itemFive.getId())};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        ItemFive delete = tracker.findById(itemFive.getId());
+        assertThat(null, is(delete));
     }
 }

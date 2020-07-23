3eb859c05d9edc4fb0c285c516c0bd04bfa175a8
Vladimir <budnichik@gmail.com>
2020-07-24 00:17:26 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#270623]

---
+++ src/ru/job4j/ex/Fact.java
@@ -0,0 +1,21 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        try {
+            System.out.println(calc(-1));
+        } catch (IllegalArgumentException ex) {
+            System.err.println("There is no result! " + ex);
+        }
+    }
+
+    public static int calc(int n) throws IllegalArgumentException{
+        int rsl = 1;
+        if ( n >= 0) {
+            for (int index = 1; index <= n; index++) {
+                rsl *= index;
+            }
+        } else throw new IllegalArgumentException("Errors happen");
+        return rsl;
+    }
+}

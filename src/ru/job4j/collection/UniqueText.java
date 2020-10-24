b046463dcdc83aa67286bd634768d33fd7876fde
Vladimir <budnichik@gmail.com>
2020-10-24 18:04:05 +0300

    2. Идентичные тексты. [#270597]

--- src/ru/job4j/collection/UniqueText.java
+++ src/ru/job4j/collection/UniqueText.java
@@ -2,19 +2,27 @@ package ru.job4j.collection;
 
 import java.util.HashSet;
 
+/**
+ /* Класс UniqueText проверяет уникальность теста
+ * @author Vladimir Budnichenko
+ * @since 24.10.2020
+ * @version 2
+ */
+
 public class UniqueText {
     public static boolean isEquals(String originText, String duplicateText) {
-        boolean rsl =true;
+        boolean rsl = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
-        for (String orig : origin) {
-            check.add(orig);
-            for (String dupl: text) {
-               rsl = check.contains(dupl);
-               break;
+            for (String orig : origin) {
+                check.add(orig);
             }
-        }
-        return rsl;
+                for (String dupl : text) {
+                    rsl = check.contains(dupl);
+                    if (!rsl) {
+                        break;
+                    }
+        } return rsl;
     }
 }

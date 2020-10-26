b6cc6de0e7d003258a5ccf14931b850e48174684
Vladimir <budnichik@gmail.com>
2020-10-26 20:53:48 +0200

    2. Преобразования List в Map. [#270594]

--- src/ru/job4j/collection/OrderConvert.java
+++ src/ru/job4j/collection/OrderConvert.java
@@ -3,6 +3,12 @@ package ru.job4j.collection;
 import java.util.HashMap;
 import  java.util.List;
 
+/**
+ * Класс данных, который конвертирует пользовательнские заявки List в Map
+ * @author Vladimir Budnichenko
+ * @version 1
+ */
+
 public class OrderConvert {
     public static HashMap<String, Order> process(List<Order> orders) {
         HashMap<String, Order> map = new HashMap<>();

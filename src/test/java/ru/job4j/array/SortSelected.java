ef3370068d4df730d75c52513a1b530ef0971897
vvbudnichenko <budnichik@gmail.com>
2024-06-20 20:34:42 +0300

    GPT. Нейросети спешат на помощь. [#505156 #501565]

--- src/test/java/ru/job4j/array/SortSelected.java
+++ src/test/java/ru/job4j/array/SortSelected.java
@@ -5,7 +5,6 @@ public class SortSelected {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
             int index = FindLoop.indexInRange(data, min, i,  data.length - 1);
-            // Swap the elements only if the minimum value is not already at index i
             if (index != i) {
                 int temp = data[i];
                 data[i] = data[index];

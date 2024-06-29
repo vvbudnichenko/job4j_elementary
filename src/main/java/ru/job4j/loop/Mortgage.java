92e9717b32bc1ffb5163d15164858f46f3438c6f
vvbudnichenko <budnichik@gmail.com>
2024-06-29 12:02:14 +0300

    5.7. Ипотека [#156315 #502760]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,12 +4,10 @@ public class Mortgage {
 
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        double debtPercent = 1 + (percent / 100);
-        double debtBalance = amount;
-
-        while (debtBalance > 0) {
+        while (amount > 0) {
+            double debtPercent = 1 + (percent / 100);
             year++;
-            debtBalance = debtBalance * debtPercent - salary;
+            amount = amount * debtPercent - salary;
         }
         return year;
     }

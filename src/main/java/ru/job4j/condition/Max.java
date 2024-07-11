29eb8057cb52291a1ad01cc411315aee4841fe78
vvbudnichenko <budnichik@gmail.com>
2024-07-11 20:39:30 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -5,4 +5,12 @@ public class Max {
     public static int max(int left, int right) {
         return left > right ? left : right;
     }
+
+    public static int max(int first, int second, int third) {
+        return max(max(first, second), third);
+    }
+
+    public static int max(int first, int second, int third, int forth) {
+        return max(max(max(first, second), third), forth);
+    }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -48,4 +48,45 @@ class MaxTest {
         int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void whenFirst5Second4Third3Then5() {
+        int first = 5;
+        int second = 4;
+        int third = 3;
+        int result = Max.max(first, second, third);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenFirst3Second4Third5Then5() {
+        int first = 3;
+        int second = 4;
+        int third = 5;
+        int result = Max.max(first, second, third);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenFirst1Second4Third2Then4() {
+        int first = 1;
+        int second = 4;
+        int third = 2;
+        int result = Max.max(first, second, third);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenFirst1Second4Third2Fort8Then8() {
+        int first = 1;
+        int second = 4;
+        int third = 2;
+        int forth = 8;
+        int result = Max.max(first, second, third, forth);
+        int expected = 8;
+        assertThat(result).isEqualTo(expected);
+    }
 }

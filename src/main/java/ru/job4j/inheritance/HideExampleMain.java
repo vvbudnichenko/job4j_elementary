1560e72440b39d326ff4a808246a98a9ad47200d
vvbudnichenko <budnichik@gmail.com>
2024-07-13 12:14:50 +0300

    Update

---
+++ src/main/java/ru/job4j/inheritance/HideExampleMain.java
@@ -0,0 +1,15 @@
+package ru.job4j.inheritance;
+
+public class HideExampleMain {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        Animal cat = new Cat();
+        animal.instanceInvoke();
+        cat.instanceInvoke();
+        Animal.staticInvoke();
+        Cat.staticInvoke();
+        Animal other = null;
+        other.staticInvoke();
+    }
+}
